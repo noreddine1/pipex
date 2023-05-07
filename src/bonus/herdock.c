23db070f1450113f33e1ee19a45b5e8895faa62b
Noureddine Maazouz <nmaazouz@e2r7p13.1337.ma>
2023-05-07 18:22:15 +0100

    complete heredoc and bonus

---
+++ src/bonus/herdock.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   herdock.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaazouz <nmaazouz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 13:30:12 by nmaazouz          #+#    #+#             */
+/*   Updated: 2023/05/07 18:15:39 by nmaazouz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/pipex.h"
+#include <fcntl.h>
+
+int	open_doc(char **av, int ac, int *fd_in, int *fd_out)
+{
+	const char	*file_name = "her_doc";
+	char		*line;
+
+	if (ac < 6)
+		ft_error(ERR_ARGS);
+	*fd_in = open(file_name, O_RDWR | O_CREAT, 0777);
+	if (*fd_in < 0)
+		ft_errorn();
+	*fd_out = open(av[ac - 1], O_APPEND | O_CREAT | O_RDWR, 0644);
+	if (*fd_out < 0)
+		ft_errorn();
+	line = get_next_line(std_in);
+	while (line && ft_strncmp(av[2], line, ft_strlen(av[2])))
+	{
+		write(*fd_in, line, ft_strlen(line));
+		free(line);
+		line = get_next_line(std_in);
+	}
+	close(*fd_in);
+	*fd_in = open(file_name, O_RDONLY);
+	unlink(file_name);
+	return (3);
+}
--- src/bonus/pipex_bonus.c
+++ src/bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nmaazouz <nmaazouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 13:53:15 by nmaazouz          #+#    #+#             */
-/*   Updated: 2023/05/05 17:04:16 by nmaazouz         ###   ########.fr       */
+/*   Updated: 2023/05/07 18:17:59 by nmaazouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ int	main(int ac, char **av, char **env)
 	char	*path_env;
 	char	**paths;
 	int		i;
-	
+
 	if (ac < 5)
 		ft_error(ERR_ARGS);
-	open_files(av, ac, &fd_in, &fd_out);
+	i = 2;
+	if (ft_strncmp(av[0], "here_doc", 9))
+		i = open_doc(av, ac, &fd_in, &fd_out);
+	else
+		open_files(av, ac, &fd_in, &fd_out);
 	path_env = get_path(env);
 	if (path_env == NULL)
 		ft_error("path env not found");
@@ -31,12 +35,8 @@ int	main(int ac, char **av, char **env)
 		ft_error("split error");
 	ft_dup2(fd_in, std_in);
 	ft_dup2(fd_out, std_out);
-	i = 2;
 	while (i < ac - 2)
-	{
-		redirecte(av[i], env, paths);
-		i++;
-	}
+		redirecte(av[i++], env, paths);
 	execute(av[i], env, paths);
-	return 0;
+	return (0);
 }
